5a73be6f48aa9e462ca659feac132f89f526333e
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-06-17 16:05:37 +0300

    Refactor code. Code status work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,81 +1,81 @@
 "use strict";
 
 //Простой обработчик событий
-/*
- btn.onclick = function(){
-    alert("TEST button");
-}
- */
 
-// let i = 0;
-// const eventTarget = (e) => {
-//   // console.log(e.target);
-//   i++;
-//   if (i == 1) {
-//     btn.removeEventListener("click", eventTarget);
-//   }
-// };
+//  btn.onclick = function(){
+//     alert("TEST button");
+// }
+ 
+
+let i = 0;
+const eventTarget = (e) => {
+  // console.log(e.target);
+  i++;
+  if (i == 1) {
+    btn.removeEventListener("click", eventTarget);
+  }
+};
 
 //Кнопки
 
-// const btn = document.querySelector("button"),
-//   btn1 = document.getElementById("btn1");
+const btn = document.querySelector("button"),
+  btn1 = document.getElementById("btn1");
 
-// btn.addEventListener("click", eventTarget);
+btn.addEventListener("click", eventTarget);
 
-// const link = document.querySelector("a");
+const link = document.querySelector("a");
 
-// link.addEventListener("click", function (event) {
-//   event.preventDefault(); //отменят стандартное событие
-//   //console.log(event.target );// создали новое событие для тега "а"
-// });
+link.addEventListener("click", function (event) {
+  event.preventDefault(); //отменят стандартное событие
+  console.log(event.target );// создали новое событие для тега "а"
+});
 
 //Удаляет кнопку
 
-// btn.addEventListener("click", (e) => {
-//   e.target.remove();
-// });
+btn.addEventListener("click", (e) => {
+  e.target.remove();
+});
 
 //Показывает информацию об элементе при наведении
 
-// btn1.addEventListener("mouseenter", (e) => {
-//   console.log(e);
-// });
+btn1.addEventListener("mouseenter", (e) => {
+  console.log(e);
+});
 
 //XMLHttpRequest
 
-// const inputRub = document.querySelector("#rub"),
-//   inputUSDT = document.querySelector("#usdt");
+const inputRub = document.querySelector("#rub"),
+  inputUSDT = document.querySelector("#usdt");
 
-// inputRub.addEventListener("input", () => {
-//   const request = new XMLHttpRequest();
+inputRub.addEventListener("input", () => {
+  const request = new XMLHttpRequest();
 
-//   //request.open(method,url,async,login,pass);
+  //request.open(method,url,async,login,pass);
 
-//   request.open("GET", "/src/scripts/curent1.json");
-//   request.setRequestHeader("Content-type", "application/json; charset=utf-8");
-//   request.send();
+  request.open("GET", "/src/scripts/curent1.json");
+  request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  request.send();
 
-  //   request.addEventListener('readystatechange',()=>{
-  //     if(request.readyState === 4 && request.status === 200){
-  //       console.log(request.response);
-  //       const data = JSON.parse(request.response);
-  //       inputUSDT.value = (+inputRub.value / data.curent.usdt).toFixed(2);
+    request.addEventListener('readystatechange',()=>{
+      if(request.readyState === 4 && request.status === 200){
+        console.log(request.response);
+        const data = JSON.parse(request.response);
+        inputUSDT.value = (+inputRub.value / data.curent.usdt).toFixed(2);
 
-  //     }else{
-  //         inputUSDT.value = "Что-то не так"
-  //     }
-  //   });
-  //
-//   request.addEventListener("load", () => {
-//     if (request.status === 200) {
-//       const data = JSON.parse(request.response);
-//       inputUSDT.value = (+inputRub.value / data.curent.usdt).toFixed(2);
-//     } else {
-//       inputUSDT.value = "Что-то не так";
-//     }
-//   });
-// });
+      }else{
+          inputUSDT.value = "Что-то не так"
+      }
+    });
+  
+  request.addEventListener("load", () => {
+    if (request.status === 200) {
+      const data = JSON.parse(request.response);
+      inputUSDT.value = (+inputRub.value / data.curent.usdt).toFixed(2);
+    } else {
+      inputUSDT.value = "Что-то не так";
+    }
+  });
+});
 
 //Promise
 //Простой пример
@@ -195,7 +195,9 @@ reqPersone.then((persone)=> {
   });
   
   */
-// const test = time => {
+
+
+  // const test = time => {
 //   return new Promise (resolve =>{
 //     setTimeout(()=> resolve(),time);
 //   });
