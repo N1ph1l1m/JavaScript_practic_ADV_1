3bd51d965f87aaa3d354648d7ef0bed0d544bc74
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-06-27 00:18:48 +0300

    Local storage final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -219,10 +219,45 @@ reqPersone.then((persone)=> {
 
       //Локальное хранилище
       /*
-      localStorage.setItem('number',5);
-      console.log(localStorage.setItem('num2',500));
-      //удаление localStorage.removeItem('number2',);
+        localStorage.setItem('number',5);
+        console.log(localStorage.setItem('num2',500));
+        //удаление localStorage.removeItem('number2',);
 
-      console.log(localStorage.getItem('number'));
+        console.log(localStorage.getItem('number'));
 
+        
+
+        const checkbox = document.querySelector("#checkbox"),
+          form = document.querySelector('form'),
+          change = document.querySelector('#color');
+
+          if(localStorage.getItem('isChecked')){
+            checkbox.checked = true ;
+          }
+          if(localStorage.getItem('bg') === 'changed'){
+            form.style.backgroundColor = '#red';
+          } 
+          checkbox.addEventListener('change', ()=>{
+            localStorage.setItem('isChecked', true);
+          });
+
+          change.addEventListener('click', ()=>{
+            if(localStorage.getItem('bg') === 'changed'){
+              localStorage.removeItem('bg');
+              form.style.backgroundColor = '#fff';
+            }else{
+              localStorage.setItem('bg','changed');
+              form.style.backgroundColor = 'red';
+            }
+          });
+
+          const persone = {
+            name: 'Alex',
+            age : 25
+          };
+
+          const serializePersone = JSON.stringify(persone);
+          localStorage.setItem('alex', serializePersone);
+
+          console.log(JSON.parse(localStorage.getItem('alex')));
       */
